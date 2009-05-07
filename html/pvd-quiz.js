abc0c23ffbec5eda77a0e58a29adaff151aea158
dr-berlin <info1@raisin.de>
2009-05-07 12:08:34 +0200

    More JS, more CSS

--- html/pvd-quiz.js
+++ html/pvd-quiz.js
@@ -9,7 +9,15 @@
             return quiz[qid].length;
         };
 
-        var getEntryHtml= function(qentry_i) {
+        var asHtml= function(qtype, qauthor, qcontent) {
+            return '<div class="qentry">'
+                +   '<div class="qtype"><div class="qtype-i">' + qtype + '</div></div>'
+                +   '<div class="qauthor">' + qauthor + '</div>'
+                +   '<div class="qcontent">' + qcontent + '</div>'
+                + '</div>';
+        };
+
+        var getEntry= function(qentry_i) {
             var qentry=  quiz[qid][qentry_i];
             var qtype=   qentry[0];
             var qauthor= qentry[1];
@@ -23,21 +31,76 @@
                 if (qptype == '-') class_= ' class="li"';
                 qcontent.push('<p', class_, '>', qpcontent, '</p>'); 
             }
-            return '<div class="qentry">'
-                +   '<div class="qtype">' + qtype + '</div>'
-                +   '<div class="qauthor">' + qauthor + '</div>'
-                +   '<div class="qcontent">' + qcontent.join('') + '</div>'
-                + '</div>';
-        }
+            return [ qtype, qauthor, qcontent.join('') ];
+        };
 
+        var getEntries= function() {
+            var result= [];
+            for (var i= quiz[qid].length - 1; i >= 0; i--) {
+                result.unshift(getEntry(i));
+            }
+            return result;
+        };
+
+        var getEntryHtml= function(qentry_i) {
+            var qentry= getEntry(qentry_i);
+            return asHtml(qentry[0], qentry[1], qentry[2]);
+        };
+
+        var getAnswerHtml= function(qentry_i) {
+            var entries= getEntries();
+            var navHtml= [];
+            for (var i= 0; i < entries.length; i++) {
+                var qentry= entries[i];
+                var class_= i == qentry_i ? 'active' : 'other';
+                navHtml.push('<span class="qtype-nav ', class_, '">', qentry[0], '</span>');
+            }
+            var qentry= entries[qentry_i];
+            if (qentry_i == 0) qentry[1]= qentry[2]= '&nbsp;';
+            return asHtml(navHtml.join(''), qentry[1], qentry[2]);
+        };
+
+        // Public methods
         this.getEntryCount= getEntryCount;
         this.getEntryHtml= getEntryHtml;
+        this.getAnswerHtml= getAnswerHtml;
+        // this.asHtml= asHtml;
+        // this.getEntries= getEntries;
         return this;
     };
 
-    $(document).ready(function() {
-        var q= Q(qid);
-        $('#qentry').html(q.getEntryHtml(0));
-    });
+var db = google.gears.factory.create('beta.database');
+db.open('database-test');
+db.execute('create table if not exists Test' +
+           ' (Phrase text, Timestamp int)');
+db.execute('insert into Test values (?, ?)', ['Monkey!', new Date().getTime()]);
+var rs = db.execute('select * from Test order by Timestamp desc');
+
+while (rs.isValidRow()) {
+  console.log(rs.field(0) + '@' + rs.field(1));
+  rs.next();
+}
+rs.close();
+
+    var q= Q(qid);
+    var entry_n= 1;
+
+    var nextA= function() {
+        entry_n= entry_n >= q.getEntryCount() - 1 ? 0 : entry_n + 1;
+        $('#qentrya').html(q.getAnswerHtml(entry_n));
+    };
+
+    $(document)
+        .bind('keydown', 'space', function(evt) {
+console.log("hu");
+            nextA();
+            return false; // don't bubble
+        })
+        .ready(function() {
+            $('#qentryq').html(q.getEntryHtml(0));
+            entry_n= -1;
+            nextA();
+        })
+    ;
 
 })(jQuery);

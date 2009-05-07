2393bd8a01c6781f12ef10dcef49c7bf320f75b3
dr-berlin <info1@raisin.de>
2009-05-07 17:09:37 +0200

    Player selection + Misc

--- html/pvd-quiz.js
+++ html/pvd-quiz.js
@@ -72,13 +72,23 @@ jQuery(function($) {
     };
 
     var Qs= function() {
+        var count= 0;
 
         var getQ= function(q_i) {
             return new Q(q_i);
         };
 
+        var getCount= function() {
+            if (count) return count;
+            for (var dummy in quiz) {
+                count++;
+            }
+            return count;
+        };
+
         // Public methods
         this.getQ= getQ;
+        this.getCount= getCount;
         return this;
     }
 
@@ -145,7 +155,7 @@ jQuery(function($) {
     //   Users class
     // ========================================================================
 
-    var Users= function(db) {
+    var Users= function(db, qs) {
 
         var getByName= function(name) {
             return db.fetchOne('SELECT * FROM user WHERE name = ?', name);
@@ -160,29 +170,51 @@ jQuery(function($) {
             db.execute('DELETE FROM known WHERE user_id = ?', userId);
         };
 
+        var getAll= function() {
+            return db.fetch('SELECT * FROM user ORDER BY name');
+        };
+
         var updateList= function() {
-            var users= db.fetch('SELECT * FROM user ORDER BY name');
+            var users= getAll();
             var html= [];            
+
+            html.push('<tr>',
+                '<th>Spielt mit?</th>',
+                '<th>Name</th>',
+                '<th>Gel&ouml;st</td>',
+                '<th>%</td>',
+                '<th></td>',
+                '</tr>');
             for (var user_i in users) {
                 var user= users[user_i];
+                var chid= 'user' + user.user_id + '_plays';
                 html.push('<tr>',
+                    '<td class="checkbox-inside"><input type="checkbox" id="' + chid + '" /></td>',
                     '<td>', user.name, '</td>',
                     '<td>', user.known_count, '</td>',
+                    '<td>', Math.floor(1000 * user.known_count / qs.getCount()) / 10, '%</td>',
                     '<td><a href="#user_remove:id=', user.user_id, ':kcount=', user.known_count, '">L&ouml;schen</a></td>',
                     '</tr>');
             }
-            $('#users').html('<table>' + html.join('') + '</table>');
+            $('#users').html('<table class="std">' + html.join('') + '</table>');
         };
 
         updateList();
 
         // Public methods
         this.getByName= getByName;
+        this.getAll= getAll;
         this.add= add;
         this.remove= remove;
         this.updateList= updateList;
         return this;
     };
+
+    $('.checkbox-inside').live('click', function(ev) {
+        if (ev.target.tagName == 'INPUT') return true;
+        $('input[type=checkbox]', this).click();
+        return false;
+    });
 
     // ========================================================================
     //   Event handler
@@ -225,7 +257,10 @@ jQuery(function($) {
     var error= function(message) {
 
         // TODO: yuck, alerts! make pretty
-        alert(message + "!");
+        alert(message
+            .replace(/&szlig;/, 'ss')
+            .replace(/&([aouAOU])uml;/g, function(match, sub1) { return sub1 + 'e'; })
+            + "!");
     };
 
     cmds.user_add= function(params) {
@@ -244,10 +279,39 @@ jQuery(function($) {
     };
 
     cmds.user_remove= function(params) {
-        if (parseInt(params.kcount) && !confirm("Wirklich loeschen?")) return;
+        if (parseInt(params.kcount) && !confirm("Wirklich l&ouml;schen?")) return;
 
         users.remove(params.id);
         users.updateList();
+    };
+
+    cmds.quiz_start= function(params) {
+        var userlist= users.getAll();
+        if (userlist.length == 0) {
+            error("Bitte erst Spieler anlegen");
+            return;
+        }
+        var players= [];
+        for (var i in userlist) {
+            var user= userlist[i];
+            if ($('#user' + user.user_id + '_plays').attr('checked')) players.push(user.user_id);
+        }
+        if (players.length == 0) {
+            error("Bitte erst Mitspieler ausw&auml;hlen");
+            return;
+        }
+        game= new Game(players);
+        updateUi();
+    };
+
+    function Game(players) {
+
+
+        return this;
+    };
+
+    function updateUi() {
+//        if (game) {
     };
 
     // ========================================================================
@@ -255,12 +319,15 @@ jQuery(function($) {
     // ========================================================================
 
     var db= new Db();
-    var users= new Users(db);
     var qs= new Qs();
+    var users= new Users(db, qs);
+    var game;
 
     var qid= "1169";
     var q= qs.getQ(qid);
     var entry_n= 1;
+
+    $('.quiz-count').html(qs.getCount());
 
     var nextA= function() {
         entry_n= entry_n >= q.getEntryCount() - 1 ? 0 : entry_n + 1;
